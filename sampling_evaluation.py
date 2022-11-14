27c15c97f2b6a543877d946ab719291437750129
David Kuska <david.kuska@student.hpi.uni-potsdam.de>
2022-11-14 16:37:04 +0100

    Added multi file sampling and evaluations

--- sampling_evaluation.py
+++ sampling_evaluation.py
@@ -1,19 +1,20 @@
 import os, csv, random, math, json, sys, datetime, uuid
+import argparse
+import itertools
 import pandas as pd
 
 ## GLOBAL CONFIGURATION PARAMETERS
 # Sampling settings
-arity               = ['unary', 'nary'][1]
-sampling_mode       = ['single_file', 'all_files'][0] #TODO: Actually implement all_files
-sampling_rates      = [0.1, 0.01, 0.001]
-sampling_methods    = ['random', 'first', 'evenly-spaced']
-# sampling_rates      = [0.1]
-# sampling_methods    = ['evenly-spaced']
+arity               = ['unary', 'nary'][0]
+# sampling_rates      = [0.1, 0.01, 0.001]
+# sampling_methods    = ['random', 'first', 'evenly-spaced']
+sampling_rates      = [0.1]
+sampling_methods    = ['evenly-spaced']
 
 header              = False
 clip_output         = True
 print_inds          = False
-create_plots        = True
+create_plots        = False
 
 # Paths, these dirs are assumed to already exist
 now = datetime.datetime.now()
@@ -24,10 +25,10 @@ results_folder      = 'results/'
 result_suffix       = '_inds'
 output_folder       = 'output/'
 output_file         = f'output_{arity}_{now.year}{now.month}{now.day}_{now.hour}{now.minute}{now.second}.csv'
-plot_folder         = 'plots'
+plot_folder         = 'plots/'
 
-# It does not really matter, how you set this parameter. Just needs to be globally defined for create_evaluation_result_csv to access it
-baseline_identifier = 'baseline_None_1'
+# # It does not really matter, how you set this parameter. Just needs to be globally defined for create_evaluation_result_csv to access it
+# baseline_identifier = 'baseline_None_1'
 
 ## Sample a single file with a certain method and rate and create a new tmp file
 # TODO: Add support for headers
@@ -78,21 +79,24 @@ def call_metanome_cli(file_name_list, output_fname='', clip_output=clip_output):
                     --files {file_name_list} \
                     --separator \; \
                     --file-key INPUT_FILES \
-                    --skip-differing-lines'
+                    --skip-differing-lines '
     if output_fname != '':
         execute_str += f' -o file:{output_fname}'     
     if arity == 'nary':
         execute_str += f' --algorithm-config DETECT_NARY:true'            
     if clip_output:
-        execute_str += '| tail -n 2'                
+        execute_str += ' | tail -n 2'                
     os.system(execute_str)
     
 ## Parses result file and returns list with string of the form 'dependentTable.dependentColumn[=referencedTable.referencedColumn'
 def parse_results(result_file):
     ind_list = []
     lines = []
-    with open(os.path.join(os.getcwd(), results_folder, result_file), 'r') as file:
-        lines = file.readlines()
+    try:
+        with open(os.path.join(os.getcwd(), results_folder, result_file), 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError as e:
+        return ind_list
 
     for line in lines:
         line_json = json.loads(line)
@@ -128,7 +132,6 @@ def parse_results(result_file):
 
         ind_list.append(ind)
     
-    
     if print_inds:
         print(ind_list)
     
@@ -144,19 +147,31 @@ def create_evaluation_result_csv(eval, baseline_identifier, output_file):
     
     with open(output_path, 'w') as csv_output:
         writer = csv.writer(csv_output, quoting=csv.QUOTE_ALL)
-        writer.writerow(['sampled_file', 'sampling_method', "sampling_rate", 'num_inds', 'tp', 'fp', 'fn'])
+        writer.writerow(['sampled_files', 'sampling_method', "sampling_rate", 'tp', 'fp', 'fn'])
         
         for key, item in eval.items(): 
-            sampled_file, sampling_method, sampling_rate = key.split('_')
+            sampled_file_paths = key.split(' ')
+            sampled_file_names = [path.rsplit('/', 1)[1].replace('.csv', '') for path in sampled_file_paths]
+            
+            file_names, methods, rates = [],[],[]
+            for sampled_file in sampled_file_names:
+                split_filename = sampled_file.split('_')
+                if len(split_filename) == 3:
+                    fname, sampling_rate, sampling_method = split_filename
+                else:
+                    fname, sampling_rate, sampling_method  = sampled_file, '1.0', 'None'
+                    
+                file_names.append(fname)
+                methods.append(sampling_method)
+                rates.append(sampling_rate)
 
-            num_inds = len(item)
             tp, fp = 0, 0
     
             for i in item:
                 if i in baseline_entry: tp += 1
                 else: fp += 1
             
-            writer.writerow([sampled_file, sampling_method, sampling_rate, num_inds, tp, fp, baseline_num_inds - tp])
+            writer.writerow(['; '.join(file_names), '; '.join(methods), '; '.join(rates), tp, fp, baseline_num_inds - tp])
 
 def clean_tmp_csv(tmp_folder):
     csv_files = [f for f in os.listdir(tmp_folder) if f.rsplit('.')[1] == 'csv']
@@ -175,38 +190,39 @@ def make_plots(output_file, plot_folder):
     pass
 
 def run():
-    evaluation_results = {}
-    
-    source_files = [os.path.join(os.getcwd(), source_dir, f) for f in os.listdir(os.path.join(os.getcwd(), source_dir)) if f.rsplit('.')[1] == 'csv']
-    ## No Sampling - Baseline
-    current_files_str = ' '.join(source_files)
-    output_fname = str(uuid.uuid4())
-    call_metanome_cli(current_files_str, output_fname=output_fname)
-    evaluation_results[baseline_identifier] = parse_results(output_fname + result_suffix)
-    
+    clean_tmp_csv(tmp_folder)
     clean_results(results_folder)
-
-    ### Sample each file
-    for file_path in source_files:
+    
+    experiments = {}
+    source_files = [os.path.join(os.getcwd(), source_dir, f) for f in os.listdir(os.path.join(os.getcwd(), source_dir)) if f.rsplit('.')[1] == 'csv']
+    baseline_identifier = " ".join(source_files)
+    
+    samples = [[src_file] for src_file in source_files]
+    ### Sample each source file with each sampling configuration
+    for i, file_path in enumerate(source_files):
         for sampling_method in sampling_methods:
             for sampling_rate in sampling_rates:
                 ### Sample
                 new_file_name = sample_csv(file_path, sampling_method, sampling_rate)
-                all_but_current_files = [os.path.join(source_dir, file) for file in source_files if file != file_path]
-                current_files_str = ' '.join(all_but_current_files)
-                current_files_str += ' ' + new_file_name
+                samples[i].append(new_file_name)
                 
-                output_fname = str(uuid.uuid4())
-                if print_inds:
-                    print(f'file_path      : {file_path}')
-                    print(f'sampling_method: {sampling_method}')
-                    print(f'sampling_rate  : {sampling_rate}')
-                    print(f'output_fname   : {output_fname}')
-                ### Execute
-                call_metanome_cli(current_files_str, output_fname)
-                ### Parse Results
-                experiment_identifier = f'{file_path.rsplit("/", 1)[1].rsplit(".", 1)[0]}_{sampling_method}_{sampling_rate}'
-                evaluation_results[experiment_identifier] = parse_results(output_fname + result_suffix)
+    ### Build cartesian product of all possible file combinations
+    ### And run experiment for each
+    for file_combination in itertools.product(*samples):
+        current_files_str = " ".join(file_combination)
+        
+        output_fname = str(uuid.uuid4())
+        if print_inds:
+            print(f'current_files_str : {current_files_str}')
+            print(f'output_fname   : {output_fname}')
+        ### Execute
+        call_metanome_cli(current_files_str, output_fname)
+        experiments[current_files_str] = output_fname
+        
+    ### Parse all results
+    evaluation_results = {}
+    for experiment_identifier, output_fname in experiments.items():        
+        evaluation_results[experiment_identifier] = parse_results(output_fname + result_suffix)
                 
     ### Persist experiment identifiers    
     create_evaluation_result_csv(evaluation_results, baseline_identifier, output_file)
