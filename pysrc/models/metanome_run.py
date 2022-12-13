e9819ec7ba5982e52c9cb3c7cd1647f702ca7f21
Clemens <68013019+ClFeSc@users.noreply.github.com>
2022-12-13 20:27:19 +0100

    Allow sampling & evaluating multiple datasets/runs at once (#23)
    * Allow running multiple dataset sampling & Metanome runs at once
    
    * Fix incorrect split at `_` instead of `__`
    
    * Add ability to load multiple `.json` files in evaluation
    
    * Add documentation about `--pipe`

--- pysrc/models/metanome_run.py
+++ pysrc/models/metanome_run.py
@@ -168,12 +168,12 @@ def parse_results(result_file_name: str, arity: str, results_folder: str, print_
 
         elif arity == 'unary' and is_baseline == False:
             dependant_raw = line_json['dependant']['columnIdentifiers'][0]
-            dependant_table = dependant_raw['tableIdentifier'].rsplit('.', 1)[0].split('_', 1)[0]
+            dependant_table = dependant_raw['tableIdentifier'].rsplit('.', 1)[0].split('__', 1)[0]
             dependant_column = 'column' + str(dependant_raw['tableIdentifier'].rsplit('.', 1)[0].rsplit('_')[-1])
             dependant = ColumnInformation(table_name=dependant_table, column_name=dependant_column)
 
             referenced_raw = line_json['referenced']['columnIdentifiers'][0]
-            referenced_table = referenced_raw['tableIdentifier'].rsplit('.', 1)[0].split('_', 1)[0]
+            referenced_table = referenced_raw['tableIdentifier'].rsplit('.', 1)[0].split('__', 1)[0]
             referenced_column = 'column' + str(referenced_raw['tableIdentifier'].rsplit('.', 1)[0].rsplit('_')[-1])
             referenced = ColumnInformation(table_name=referenced_table, column_name=referenced_column)
 
@@ -207,7 +207,7 @@ def parse_results(result_file_name: str, arity: str, results_folder: str, print_
             dependant_list = []
             dependant_raw = line_json['dependant']['columnIdentifiers']
             for dependant_entry in dependant_raw:
-                dependant_table = dependant_entry['tableIdentifier'].rsplit('.', 1)[0].split('_', 1)[0]
+                dependant_table = dependant_entry['tableIdentifier'].rsplit('.', 1)[0].split('__', 1)[0]
                 dependant_column = 'column' + str(dependant_entry['tableIdentifier'].rsplit('.', 1)[0].rsplit('_')[-1])
                 dependant = ColumnInformation(table_name=dependant_table, column_name=dependant_column)
                 # dependant_list.append(f'{dependant_table}.{dependant_column}')
@@ -216,7 +216,7 @@ def parse_results(result_file_name: str, arity: str, results_folder: str, print_
             referenced_list = []
             referenced_raw = line_json['referenced']['columnIdentifiers']
             for referenced_entry in referenced_raw:
-                referenced_table = referenced_entry['tableIdentifier'].rsplit('.', 1)[0].split('_', 1)[0]
+                referenced_table = referenced_entry['tableIdentifier'].rsplit('.', 1)[0].split('__', 1)[0]
                 referenced_column = 'column' + str(referenced_entry['tableIdentifier'].rsplit('.', 1)[0].rsplit('_')[-1])
                 referenced = ColumnInformation(table_name=referenced_table, column_name=referenced_column)
                 # referenced_list.append(f'{referenced_table}.{referenced_column}')
@@ -235,7 +235,7 @@ def parse_results(result_file_name: str, arity: str, results_folder: str, print_
     return MetanomeRunResults(ind_list)
 
 
-def run_metanome(configuration: MetanomeRunConfiguration, output_fname: str) -> MetanomeRun:
+def run_metanome(configuration: MetanomeRunConfiguration, output_fname: str, pipe: bool) -> MetanomeRun:
     # Make these configurable
     metanome_cli_path = 'metanome-cli.jar'
     algorithm_path = 'BINDER.jar'
@@ -257,7 +257,9 @@ def run_metanome(configuration: MetanomeRunConfiguration, output_fname: str) -> 
                     -o {output_rule} \
                     --escape {escape} \
                     --algorithm-config DETECT_NARY:{"true" if configuration.arity == "nary" else "false"}'
-    if configuration.clip_output:
+    if pipe:
+        execute_str += ' | tail -n 0'
+    elif configuration.clip_output:
         execute_str += ' | tail -n 2'
     # Run
     os.system(execute_str)
