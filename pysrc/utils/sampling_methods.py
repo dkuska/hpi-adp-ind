0c85f2e654ef1ebc833a92340e10287ae56c6456
Yorick <yorick.scheffler@gmail.com>
2023-01-08 13:10:19 +0100

    Some changes were missing

--- pysrc/utils/sampling_methods.py
+++ pysrc/utils/sampling_methods.py
@@ -13,14 +13,25 @@ def random_sample(data: list[list[str]],
     tmp = pd.Series(data)
     tmp = replaceEmptyField(tmp)
     tmp.dropna(inplace=True)
-    return tmp.sample(n=num_samples)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
 
+    if len(tmp)-1 > num_samples:
+        return tmp.sample(n=num_samples)
+    else:
+        return tmp
 
 def first_sample(data: list[list[str]],
                  num_samples: int,
                  num_entries: int) -> pd.Series:
     tmp = pd.Series(data)
-    return tmp.iloc[:num_samples]
+    tmp = replaceEmptyField(tmp)
+    tmp.dropna(inplace=True)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
+
+    if len(tmp) - 1 > num_samples:
+        return tmp.iloc[:num_samples]
+    else:
+        return tmp
 
 def smallest_value_sample(data: list[list[str]],
                  num_samples: int,
@@ -28,29 +39,40 @@ def smallest_value_sample(data: list[list[str]],
     tmp = pd.Series(data)
     tmp = replaceEmptyField(tmp)
     tmp.dropna(inplace=True)
-    out = tmp.sort_values()
-    return out.iloc[:num_samples]
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
 
+    out = tmp.sort_values()
+    if len(out) - 1 > num_samples:
+        return out.iloc[:num_samples]
+    else:
+        return out
 def biggest_value_sample(data: list[list[str]],
                  num_samples: int,
                  num_entries: int) -> pd.Series:
     tmp = pd.Series(data)
     tmp = replaceEmptyField(tmp)
     tmp.dropna(inplace=True)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
+
     out = tmp.sort_values(ascending=False)
-
-    return out.iloc[:num_samples]
-
+    if len(out) - 1 > num_samples:
+        return out.iloc[:num_samples]
+    else:
+        return out
 def longest_value_sample(data: list[list[str]],
                  num_samples: int,
                  num_entries: int) -> pd.Series:
     tmp = pd.Series(data)
     tmp = replaceEmptyField(tmp)
     tmp.dropna(inplace=True)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
+
     tmp.index = tmp.str.len()
     df = tmp.sort_index(ascending=False).reset_index(drop=True)
-
-    return df.iloc[:num_samples]
+    if len(df) - 1 > num_samples:
+        return df.iloc[:num_samples]
+    else:
+        return df
 
 def shortest_value_sample(data: list[list[str]],
                  num_samples: int,
@@ -58,27 +80,39 @@ def shortest_value_sample(data: list[list[str]],
     tmp = pd.Series(data)
     tmp = replaceEmptyField(tmp)
     tmp.dropna(inplace=True)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
+
     tmp.index = tmp[0].str.len()
     df = tmp.sort_index(ascending=True).reset_index(drop=True)
+    if len(df) - 1 > num_samples:
+        return df.iloc[:num_samples]
+    else:
+        return df
 
-    return df.iloc[:num_samples]
-
+#Probably the same as first now and therefore obsolete?
 def all_distinct_sample(data: list[list[str]],
                  num_samples: int,
                  num_entries: int) -> pd.Series:
     tmp = pd.Series(data)
     df = tmp.loc[tmp.astype(str).drop_duplicates().index]
-
-
     return df.iloc[:num_samples]
 
+#only method were I had to make it dependent on sample size after deduplicating
+#no idea how to keep it there because at some points it tries to sample from spaces that doesn't exist
+#doesnt't work?
 def evenly_spaced_sample(data: list[list[str]],
                          num_samples: int,
                          num_entries: int) -> pd.Series:
-    space_width = math.ceil(num_entries / num_samples)
-    starting_index = random.randint(0, space_width)
     tmp = pd.Series(data)
-    out_indices = [i % num_entries for i in range(starting_index, num_entries + space_width, space_width)]
+    tmp = replaceEmptyField(tmp)
+    tmp.dropna(inplace=True)
+    tmp = tmp.loc[tmp.astype(str).drop_duplicates().index]
+    rate = num_samples / num_entries
+    num_samples_here = math.ceil(len(tmp) * rate)
+    space_width = math.ceil(len(tmp) / num_samples_here)
+    starting_index = random.randint(0, space_width)
+    out_indices = [i % num_entries for i in range(starting_index, num_samples_here + space_width - 1, space_width)]
+
     out = tmp.iloc[out_indices]
     return out
 
