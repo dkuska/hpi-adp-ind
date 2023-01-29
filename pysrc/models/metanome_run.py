7299e3caec0d7326da1a1d70f5998ede6c83b00b
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2023-01-29 15:35:59 +0100

    Present INDs that were not found by any non-baseline run with credibility -2.0

--- pysrc/models/metanome_run.py
+++ pysrc/models/metanome_run.py
@@ -163,13 +163,14 @@ class MetanomeRunBatch:
     def ranked_inds(self) -> dict[IND, float]:
         # Collect INDs
         ind_map: dict[tuple[str, str], IND] = {}  # Map from (dependent, referenced) -> IND
+        inds: dict[IND, list[tuple[int, MetanomeRun]]] = {}
         for run in self.runs:
             for ind in run.results.inds:
                 if (str(ind.dependents), str(ind.referenced)) not in ind_map:
                     clean_ind = IND(dependents=ind.dependents, referenced=ind.referenced)
                     ind_map[(str(ind.dependents), str(ind.referenced))] = clean_ind
+                    inds[clean_ind] = []
 
-        inds: dict[IND, list[tuple[int, MetanomeRun]]] = {}
         for run in self.runs:
             # Skip baseline (it won't be available in production)
             # TODO: Is this correct behavior? Not sure, but otherwise we'll get *every* TP IND as we're checking the baseline.
@@ -177,7 +178,7 @@ class MetanomeRunBatch:
                 continue
             for ind in run.results.inds:
                 clean_ind = ind_map[(str(ind.dependents), str(ind.referenced))]
-                if clean_ind not in inds: inds[clean_ind] = []
+                # if clean_ind not in inds: inds[clean_ind] = []
                 inds[clean_ind].append((next(error['missing_values'] for error in ind.errors if isinstance(error, dict) and 'missing_values' in error), run))
         # maximum_missing_values = max(missing_values for configMissingValuePairs in inds.values() for missing_values, _ in configMissingValuePairs)
         baseline = self.baseline
@@ -190,6 +191,11 @@ class MetanomeRunBatch:
                 for ind, configMissingValuesPairs
                 in inds.items()
             }
+        for ind in inds_credibilities:
+            if len(inds_credibilities[ind]) > 0:
+                continue
+            # When this IND was not found in the runs, set -2.
+            inds_credibilities[ind] = [-2.0]
         # Rank INDs by SUM over ALL runs (with value 0.0 for runs that didn't find the IND)
         ranked_inds = { ind: credibility_sum if not isnan(credibility_sum := sum(credibilities)) else -1.0 for ind, credibilities in inds_credibilities.items() }
         return ranked_inds
