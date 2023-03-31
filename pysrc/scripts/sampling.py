ffdceb4b9022f6a9ab2845d474b25e365af43df2
Yorick <yorick.scheffler@gmail.com>
2023-03-31 15:16:14 +0200

    Implements requested changes

--- pysrc/scripts/sampling.py
+++ pysrc/scripts/sampling.py
@@ -42,20 +42,21 @@ def assign_budget(size_per_column: list[list[ColumnBudgetInfo]], budget_to_share
     )
 
     # budget that was left after the initial phase, which is now distributed between the columns that require more budget
-    budget_per_column = math.floor(budget_to_share / count_columns_not_full)
+    budget_per_column = 0
+    if count_columns_not_full != 0:
+        budget_per_column = math.floor(budget_to_share / count_columns_not_full)
+
+
 
     # Probably not even required adds another break condition if all columns are at the final budget stage and in between
     # two function calls no more adaptions to the budget could be made
-    if count_columns_not_full == track_changes:
+    if count_columns_not_full == track_changes or budget_per_column == 0:
         for sizes_for_file in size_per_column:
             for size_for_column in sizes_for_file:
                 if not size_for_column.full_column_fits_in_budget:
                     size_for_column.allowed_budget = budget_per_column + basic_size
         return size_per_column
 
-    # no more budget is available and can be shared between columns
-    if budget_per_column == 0:
-        return size_per_column
 
     for sizes_for_file in size_per_column:
         for size_for_column in sizes_for_file:
@@ -72,11 +73,14 @@ def assign_budget(size_per_column: list[list[ColumnBudgetInfo]], budget_to_share
                 # needs no adaption because all uniques can be fitted into the budget
                 else:
                     size_for_column.full_column_fits_in_budget = True
-                    budget_to_share += (budget_per_column + basic_size)-size_for_column.allowed_budget
+                    budget_to_share += basic_size-size_for_column.allowed_budget
 
 
-    # calls the function again and checks if the now left budget can be even further distributed to columns that require more budget
-    return assign_budget(size_per_column, budget_to_share, basic_size+budget_per_column, count_columns_not_full)
+
+
+    # calls the function again and checks if the now left budget can be even further distributed to columns that require more budget'
+    return assign_budget(size_per_column, budget_to_share, basic_size + budget_per_column, count_columns_not_full)
+
 
 def sample_csv(file_path: str,
                sampling_method: str,
@@ -278,7 +282,9 @@ def run_experiments(dataset: str, config: GlobalConfiguration) -> str:
             size_per_column: list[list[ColumnBudgetInfo]] = [[] for _ in range(len(source_files))]
             # Calculates the budget per Column if all column would get the same budget
             column_count = sum(len(file) for file in description)
+
             basic_size = math.floor(budget/column_count)
+
             for file_index, file_description in enumerate(description):
                 for column_index, column_description in enumerate(file_description):
                     # Checks if the basic size is enough to represent all unique values
@@ -295,6 +301,14 @@ def run_experiments(dataset: str, config: GlobalConfiguration) -> str:
             # After the initial budget per columns it's necessary to split the pool of budget that wasn't required by some
             # columns and split it evenly to the columns that need more budget
             size_per_column = assign_budget(size_per_column, budget_to_share, basic_size, track_changes=0)
+
+            #budget_used = sum(
+            #    size_for_column.allowed_budget
+            #    for sizes_for_file
+            #    in size_per_column
+            #    for size_for_column
+            #    in sizes_for_file)
+
 
             for i, file_path in enumerate(source_files):
                 new_file_list.extend(sample_csv(file_path, sampling_method, budget, size_per_column[i], config))
